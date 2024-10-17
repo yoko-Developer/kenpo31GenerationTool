c518a569aadb36c50512d19aa5c6152ae8df7dd8
yoko morioka <whitesnow.wave@gmail.com>
2024-10-17 23:13:10 +0900

    Add files while respecting .gitignore

---
+++ ui/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace kenpo31GenerationTool
+{
+	partial class Form1
+	{
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox txtFilePath;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.Button btnConvert;
+		private System.Windows.Forms.Label lblFilePath;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		/// <summary>
+		/// コンポーネントの初期化
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtFilePath = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.lblFilePath = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+
+			// txtFilePath
+			this.txtFilePath.Location = new System.Drawing.Point(12, 29);
+			this.txtFilePath.Name = "txtFilePath";
+			this.txtFilePath.Size = new System.Drawing.Size(260, 20);
+			this.txtFilePath.TabIndex = 0;
+
+			// btnBrowse
+			this.btnBrowse.Location = new System.Drawing.Point(278, 27);
+			this.btnBrowse.Name = "btnBrowswe";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+
+			// btnConvert
+			this.btnConvert.Location = new System.Drawing.Point(12, 60);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(75, 23);
+			this.btnConvert.TabIndex = 2;
+			this.btnConvert.Text = "変換開始";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+
+			// lblFilePath
+			this.lblFilePath.AutoSize = true;
+			this.lblFilePath.Location = new System.Drawing.Point(12, 9);
+			this.lblFilePath.Name = "lblFilePath";
+			this.lblFilePath.Size = new System.Drawing.Size(109, 13);
+			this.lblFilePath.TabIndex = 3;
+			this.lblFilePath.Text = "FI_JRK_0004.csvのパス";
+
+			// Form1
+			this.ClientSize = new System.Drawing.Size(365, 101);
+			this.Controls.Add(this.lblFilePath);
+			this.Controls.Add(this.btnConvert);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtFilePath);
+			this.Name = "Form1";
+			this.Text = "FI_JRK_0004.csv読み込みツール";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
+
---
+++ ui/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using kenpo31GenerationTool.csvHandling; // CsvReaderの名前空間
+
+namespace kenpo31GenerationTool
+{
+	public partial class Form1 : Form
+	{
+		private CsvReader csvReader = new CsvReader();  // CsvReaderのインスタンス
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// ファイル選択ボタンをクリックした際のイベントハンドラ
+		/// </summary>
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog
+			{
+				Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*",
+				Title = "FI_JRK_0004.csvファイルを選択してください"
+			};
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				// 選択したファイルのパスをテキストボックスに表示
+				txtFilePath.Text = openFileDialog.FileName;
+			}
+		}
+
+		/// <summary>
+		/// 変換開始ボタンをクリックした際のイベントハンドラ
+		/// </summary>
+		private void btnConvert_Click(object sender, EventArgs e)
+		{
+			string filePath = txtFilePath.Text;
+
+			if (string.IsNullOrEmpty(filePath))
+			{
+				MessageBox.Show("CSVファイルのパスを指定してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				// CSVファイル読み込み
+				List<string[]> records = csvReader.ReadCsv(filePath);
+
+				if (records.Count > 0)
+				{
+					// ヘッダチェック
+					string[] expectedHeaders = { "要求レコード番号", "個人番号", "照会結果氏名", "照会結果氏名かな", "照会結果生年月日", "照会結果性別コード", "照会結果住所", "市町村コード", "対象者識別情報" };
+					if (!csvReader.ValidateHeaders(records[0], expectedHeaders))
+					{
+						MessageBox.Show("CSVファイルのヘッダが不正です。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return;
+					}
+
+					// バリデーションチェック
+					for (int i = 1; i < records.Count; i++) // ヘッダ以外の行をチェック
+					{
+						if (!csvReader.ValidateRecord(records[i]))
+						{
+							MessageBox.Show($"レコード {i + 1} でバリデーションエラーが発生しました。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+							return;
+						}
+					}
+
+					MessageBox.Show("CSVファイルの読み込みとバリデーションが成功しました。", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else
+				{
+					MessageBox.Show("CSVファイルが空です。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"エラーが発生しました: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
