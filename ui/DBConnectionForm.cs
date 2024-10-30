62addf766a0a0964f148a0ed6b15302a00cda72c
yoko morioka <whitesnow.wave@gmail.com>
2024-10-30 11:47:03 +0900

    テーブル内容をモーダルに出力する

--- ui/DBConnectionForm.cs
+++ ui/DBConnectionForm.cs
@@ -1,44 +1,109 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace kenpo31GenerationTool.ui
 {
+
+	/// <summary>
+	/// データベース接続を行い、テーブル情報を表示するためのフォーム
+	/// </summary>
 	public partial class DBConnectionForm : Form
 	{
+
+		/// <summary>
+		/// DBConnectionForm クラスの新しいインスタンスを初期化する
+		/// </summary>
 		public DBConnectionForm()
 		{
 			InitializeComponent();
 		}
 
+		/// <summary>	
+		/// データベースに接続し、テーブル情報をモーダルに表示
+		/// </summary>
 		private void BtnConnect_Click(object sender, EventArgs e)
 		{
-			string serverName = txtServerName.Text;
-			string databaseName = txtDatabaseName.Text;
-			string userId = txtUserId.Text;
-			string password = txtPassword.Text;
+			// 確認ダイアログを表示
+			var result = MessageBox.Show("接続してよろしいですか？", "接続確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-			// 接続文字列を作成
-			string connectionString = $"Server={serverName};Database={databaseName};User Id={userId};Password={password};";
-
-			try
+			// 「はい」が選択された場合のみ接続処理を行う	
+			if (result == DialogResult.Yes)
 			{
-				using (SqlConnection connection = new SqlConnection(connectionString))
+				string serverName = txtServerName.Text;
+				string databaseName = txtDatabaseName.Text;
+				string userId = txtUserId.Text;
+				string password = txtPassword.Text;
+
+				// 接続文字列を作成
+				string connectionString = $"Server={serverName};Database={databaseName};User Id={userId};Password={password};";
+
+				try
 				{
-					connection.Open();
-					MessageBox.Show("データベースに接続されました！", "接続成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					using (SqlConnection connection = new SqlConnection(connectionString))
+					{
+						connection.Open();
+						MessageBox.Show("データベースに接続されました", "接続成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+						// 各テーブルからデータを取得
+						SqlDataAdapter adapterKA11 = new SqlDataAdapter("SELECT * FROM KA11LICH", connection);
+						SqlDataAdapter adapterKA12 = new SqlDataAdapter("SELECT * FROM KA12LICF", connection);
+						SqlDataAdapter adapterAddress = new SqlDataAdapter("SELECT * FROM KAFIJADL", connection);
+
+						// 各テーブルデータを格納する DataTable を作成
+						DataTable tableKA11 = new DataTable();
+						DataTable tableKA12 = new DataTable();
+						DataTable tableAddress = new DataTable();
+
+						// 各テーブルにデータを読み込み
+						adapterKA11.Fill(tableKA11);
+						adapterKA12.Fill(tableKA12);
+						adapterAddress.Fill(tableAddress);
+
+						// 各テーブルのデータをモーダルフォームで表示
+						ShowTableData("KA11LICH", tableKA11);
+						ShowTableData("KA12LICF", tableKA12);
+						ShowTableData("KAFIJADL", tableAddress);
+					}
 				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show($"接続に失敗しました: {ex.Message}", "接続エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				catch (Exception ex)
+				{
+					MessageBox.Show($"接続に失敗しました: {ex.Message}", "接続エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
 			}
 		}
 
-		private void BtnBack_Click(object sender, EventArgs e)
+		/// <summary>
+		/// 指定されたテーブルのデータを TableDataViewerForm で表示
+		/// </summary>
+		/// <param name="tableName">表示するテーブルの名前</param>
+		/// <param name="tableData">表示するテーブルデータ</param>
+		private void ShowTableData(string tableName, DataTable tableData)
 		{
-			// 戻るボタンの処理をここに追加
+			using (var viewerForm = new TableDataViewerForm(tableName, tableData))
+			{
+				viewerForm.ShowDialog(); // モーダルで表示
+
+			}
+		}
+
+		/// <summary>	
+		/// 戻るボタンの処理でフォームを閉じて前の画面に戻る	
+		/// </summary>
+		private void BtnBack_Click(object sender, EventArgs e)
+{
 			this.Close();
+		}
+
+		private void TxtPassword_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void DBConnectionForm_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
---
+++ ui/TableDataViewerForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kenpo31GenerationTool.ui
+{
+
+	/// <summary>
+	/// 各テーブルデータを表示するためのモーダルフォーム
+	/// </summary>
+	public partial class TableDataViewerForm : Form
+	{
+		private DataGridView dataGridViewTable;
+		/// <summary>
+		/// 指定されたテーブルデータを表示するためのコンストラクタ
+		/// </summary>
+		/// <param name="tableName">テーブル名</param>
+		/// <param name="tableData">表示するテーブルのデータ</param>
+		public TableDataViewerForm(string tableName, DataTable tableData)
+		{
+			InitializeComponent();
+			this.Text = $"{tableName} のデータ";
+			// dataGridViewTableの設定
+			dataGridViewTable = new DataGridView
+			{
+				Dock = DockStyle.Fill, // フォーム全体に展開
+				DataSource = tableData,
+				AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+			};
+
+			this.Controls.Add(dataGridViewTable);
+		}
+
+		private void TableDataViewerForm_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
