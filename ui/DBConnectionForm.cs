73c26ede495c13da445560ab539c49b84b7e8584
yoko morioka <whitesnow.wave@gmail.com>
2024-10-31 23:32:35 +0900

    必要なデータを取得

--- ui/DBConnectionForm.cs
+++ ui/DBConnectionForm.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Windows.Forms;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace kenpo31GenerationTool.ui
 {
-
 	/// <summary>
 	/// データベース接続を行い、テーブル情報を表示するためのフォーム
 	/// </summary>
 	public partial class DBConnectionForm : Form
 	{
-
 		/// <summary>
 		/// DBConnectionForm クラスの新しいインスタンスを初期化する
 		/// </summary>
@@ -20,15 +18,13 @@ namespace kenpo31GenerationTool.ui
 			InitializeComponent();
 		}
 
-		/// <summary>	
-		/// データベースに接続し、テーブル情報をモーダルに表示
+		/// <summary>
+		/// データベースに接続し、必要なテーブル情報を取得して表示
 		/// </summary>
 		private void BtnConnect_Click(object sender, EventArgs e)
 		{
-			// 確認ダイアログを表示
 			var result = MessageBox.Show("接続してよろしいですか？", "接続確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-			// 「はい」が選択された場合のみ接続処理を行う	
 			if (result == DialogResult.Yes)
 			{
 				string serverName = txtServerName.Text;
@@ -36,7 +32,6 @@ namespace kenpo31GenerationTool.ui
 				string userId = txtUserId.Text;
 				string password = txtPassword.Text;
 
-				// 接続文字列を作成
 				string connectionString = $"Server={serverName};Database={databaseName};User Id={userId};Password={password};";
 
 				try
@@ -44,49 +39,53 @@ namespace kenpo31GenerationTool.ui
 					using (SqlConnection connection = new SqlConnection(connectionString))
 					{
 						connection.Open();
-						MessageBox.Show("データベースに接続されました", "接続成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-						// 各テーブルからデータを取得
-						SqlDataAdapter adapterKA11 = new SqlDataAdapter("SELECT * FROM KA11LICH", connection);
-						SqlDataAdapter adapterKA12 = new SqlDataAdapter("SELECT * FROM KA12LICF", connection);
-						SqlDataAdapter adapterAddress = new SqlDataAdapter("SELECT * FROM KAFIJADL", connection);
+						// 各テーブルから必要なデータを取得するクエリ
+						string queryKA11 = "SELECT KOJINID, SHAINCOD AS 社員コード FROM KA11LICH";
+						string queryKA14 = "SELECT KOJINID, ZOKUGARA AS 続柄コード, SEIBETU AS 性別コード, BIRTHDAY AS 生年月日 FROM KA14HFUY";
+						string queryAddress = "SELECT KOJINID, EMAIL AS メールアドレス FROM KA13HHKN";
 
-						// 各テーブルデータを格納する DataTable を作成
-						DataTable tableKA11 = new DataTable();
-						DataTable tableKA12 = new DataTable();
-						DataTable tableAddress = new DataTable();
+						// 各テーブルのデータを取得
+						DataTable tableKA11 = GetData(connection, queryKA11);
+						DataTable tableKA14 = GetData(connection, queryKA14);
+						DataTable tableAddress = GetData(connection, queryAddress);
 
-						// 各テーブルにデータを読み込み
-						adapterKA11.Fill(tableKA11);
-						adapterKA12.Fill(tableKA12);
-						adapterAddress.Fill(tableAddress);
-
-						// 各テーブルのデータをタブ表示するためのフォームを開く
-
-						using (var viewerForm = new TableDataViewerForm(tableKA11, tableKA12, tableAddress))
-						{
-								viewerForm.ShowDialog();
-							}
-						}
-					}
-					catch (Exception ex)
-					{
-						MessageBox.Show($"接続に失敗しました: {ex.Message}", "接続エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						// タブで表示するためにモーダルフォームを生成
+						TableDataViewerForm tableViewerForm = new TableDataViewerForm(tableKA11, tableKA14, tableAddress);
+						tableViewerForm.ShowDialog();
 					}
 				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"接続に失敗しました: {ex.Message}", "接続エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
 			}
+		}
+
+		/// <summary>
+		/// データを取得するヘルパーメソッド
+		/// </summary>
+		private DataTable GetData(SqlConnection connection, string query)
+		{
+			using (SqlCommand command = new SqlCommand(query, connection))
+			using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+			{
+				DataTable dataTable = new DataTable();
+				adapter.Fill(dataTable);
+				return dataTable;
+			}
+		}
 
 		/// <summary>	
 		/// 戻るボタンの処理でフォームを閉じて前の画面に戻る	
 		/// </summary>
 		private void BtnBack_Click(object sender, EventArgs e)
-{
-			this.Close();
-		}
-
-		private void DBConnectionForm_Load(object sender, EventArgs e)
 		{
+					this.Close();
+				}
 
+				private void DBConnectionForm_Load(object sender, EventArgs e)
+				{
+			}
 		}
 	}
-}
